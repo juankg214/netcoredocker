2fb3e46289f493a9f95a57829342c189b95b96a3
Juan Gomez <juankg214@gmail.com>
2024-03-06 21:50:12 -0500

    first commit

---
+++ dotnet-webapp/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using dotnet_docker_app.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace dotnet_docker_app.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _config;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _config = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.ConnectionString = _config.GetConnectionString("DefaultConnection");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
